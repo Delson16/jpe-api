4ff5b1dccfc5331a3e4466e286ccff493dbf7ed6
Delson Pilar <146981737+Delson16@users.noreply.github.com>
2025-05-29 18:35:49 -0300

    Iniciando projeto APIRest

---
+++ src/main/java/com/juntasporeles/jpe/JpeApplication.java
@@ -0,0 +1,13 @@
+package com.juntasporeles.jpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juntasporeles/jpe/JpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juntasporeles.jpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
